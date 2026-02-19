0a64ae3fb3d818444f337054f1fd6ac24a79e9bd
Yash Jain <yashjain2637@gmail.com>
2026-02-19 15:37:07 +0530

    third commit

--- main.py
+++ main.py
@@ -30,3 +30,12 @@ lr = LinearRegression()
 lr.fit(x_train, y_train)
 
 y_pred = lr.predict(x_test)
+
+from sklearn.metrics import r2_score
+print(r2_score(y_test , y_pred))
+
+from sklearn.metrics import mean_absolute_error
+print(mean_absolute_error(y_test, y_pred))
+
+from sklearn.metrics import mean_squared_error
+print(mean_squared_error(y_test,y_pred))
